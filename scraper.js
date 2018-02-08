f3adc0fb6b5bc76af8587fe448e70c29c998b018
pdxoliver <rms.moliver@gmail.com>
2018-02-08 15:08:05 -0800

    added img

--- scraper.js
+++ scraper.js
@@ -1,12 +1,14 @@
 //Dependencies:
 
-const request = require("tinyreq"),
- cheerio = require("cheerio"),
- req = require("tinyreq"),
- scrapeIt = require("scrape-it");
+const 	scrapeIt = require("scrape-it"),
+		fs = require('fs'),
+		csv = require('json2csv');
 
  // Global variables
  let shirtUrls =[];
+ let url = [];
+ let fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+ let shirtData = [];
 
 // Access the website  http://shirts4mike.com ( http://shirts4mike.com/shirts.php)
 scrapeIt('http://shirts4mike.com/shirts.php', {
@@ -14,18 +16,21 @@ scrapeIt('http://shirts4mike.com/shirts.php', {
 	shirts: {
 		listItem: '.products li',
 		data: {
-			URL: {
+			link: {
 				selector: 'a',
 				attr: 'href'
+			},
+			pic: {
+				selector: 'img',
+				attr: 'src'
 			}
 		}
 	}
 }).then(({ data, response }) => {
-	console.log(data.shirts);
 	shirtUrls = (data.shirts);
+	console.log(shirtUrls);
+	
 });
-
-
 
 
 
